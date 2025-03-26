17708bb0c2b697533b96db39b61f04faa43ec534
Flo466 <bruel.florent@gmail.com>
2025-03-26 18:28:03 +0100

    clean DB

--- src/Entity/Restaurant.php
+++ src/Entity/Restaurant.php
@@ -43,23 +43,9 @@ class Restaurant
     #[ORM\OneToMany(targetEntity: Picture::class, mappedBy: 'restaurant', orphanRemoval: true)]
     private Collection $pictures;
 
-    /**
-     * @var Collection<int, Booking>
-     */
-    #[ORM\OneToMany(targetEntity: Booking::class, mappedBy: 'restaurant')]
-    private Collection $bookings;
-
-    /**
-     * @var Collection<int, Menu>
-     */
-    #[ORM\OneToMany(targetEntity: Menu::class, mappedBy: 'restaurant')]
-    private Collection $menus;
-
     public function __construct()
     {
         $this->pictures = new ArrayCollection();
-        $this->bookings = new ArrayCollection();
-        $this->menus = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -175,66 +161,6 @@ class Restaurant
             // set the owning side to null (unless already changed)
             if ($picture->getRestaurant() === $this) {
                 $picture->setRestaurant(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Booking>
-     */
-    public function getBookings(): Collection
-    {
-        return $this->bookings;
-    }
-
-    public function addBooking(Booking $booking): static
-    {
-        if (!$this->bookings->contains($booking)) {
-            $this->bookings->add($booking);
-            $booking->setRestaurant($this);
-        }
-
-        return $this;
-    }
-
-    public function removeBooking(Booking $booking): static
-    {
-        if ($this->bookings->removeElement($booking)) {
-            // set the owning side to null (unless already changed)
-            if ($booking->getRestaurant() === $this) {
-                $booking->setRestaurant(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Menu>
-     */
-    public function getMenus(): Collection
-    {
-        return $this->menus;
-    }
-
-    public function addMenu(Menu $menu): static
-    {
-        if (!$this->menus->contains($menu)) {
-            $this->menus->add($menu);
-            $menu->setRestaurant($this);
-        }
-
-        return $this;
-    }
-
-    public function removeMenu(Menu $menu): static
-    {
-        if ($this->menus->removeElement($menu)) {
-            // set the owning side to null (unless already changed)
-            if ($menu->getRestaurant() === $this) {
-                $menu->setRestaurant(null);
             }
         }
 
